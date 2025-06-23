c7c8af6a95363a302884d4bb5b8fb5dfd40fa040
Lepp3 <lubaka05@gmail.com>
2025-06-23 11:26:34 +0300

    created initial skeleton for project

---
+++ src/assets/products.ts
@@ -0,0 +1,147 @@
+export const products = [
+  {
+    id: '1',
+    name: 'Visionary logistical process improvement',
+    category: 'Home',
+    price: 15.19,
+    image: "productImage",
+    description: 'Provide during attention charge about never sea smilefact.'
+}, {
+id: '2',
+     name: 'Face-to-face bottom-line leverage',
+    category: 'Electronics',
+    price: 198.92,
+    image: "productImage",
+    description: 'School many TV conference statement side city week.'
+  },
+  {
+    id: '3',
+    name: 'Ergonomic context-sensitive structure',
+    category: 'Home',
+    price: 54.26,
+    image: "productImage",
+    description: 'Trip raise sign amount threat store prove draw church help lot plan.'
+}, {
+    id: '4',
+    name: 'Total exuding system engine',
+    category: 'Electronics',
+    price: 82.8,
+    image: "productImage",
+    description: 'Mr economy free change message pass watch world phone must worry.'
+}, {
+    id: '5',
+    name: 'Cross-platform multimedia project',
+    category: 'Books',
+    price: 129.44,
+    image: "productImage",
+    description: 'Force morning foot art political yes forget inside human.'
+}, {
+    id: '6',
+    name: 'Centralized disintermediate structure',
+    category: 'Toys',
+    price: 137.59,
+    image: "productImage",
+    description: 'Half prove bit want argue play institution stuff personal.'
+}, {
+    id: '7',
+    name: 'Proactive bandwidth-oriented initiative',
+    category: 'Books',
+    price: 34.18,
+     image: "productImage",
+    description: 'Fall staff between surface true manage ever eye lead.'
+  },
+  {
+    id: '8',
+    name: 'Open-source bifurcated migration',
+    category: 'Toys',
+    price: 176.99,
+    image: "productImage",
+    description: 'Position stage walk politics coach baby physical change team.'
+}, {
+    id: '9',
+    name: 'Automated bandwidth-monitored capacity',
+    category: 'Clothing',
+    price: 135.4,
+    image: "productImage",
+    description: 'Laugh majority mission air nation out stuff strategy position.'
+}, {
+    id: '10',
+    name: 'Robust national alliance',
+    category: 'Books',
+    price: 130.58,
+    image: "productImage",
+    description: 'Smile amount real matter development fill career.'
+}, {
+    id: '11',
+    name: 'Intuitive uniform analyzer',
+    category: 'Home',
+    price: 94.92,
+    image: "productImage",
+    description: 'Movement gun successful forget deal develop rule budget.'
+}, {
+    id: '12',
+    name: 'Real-time content-based strategy',
+    category: 'Toys',
+    price: 57.28,
+    image: "productImage",
+    description: 'Back style experience value born control strategy success news.'
+},
+
+   {
+    id: '13',
+    name: 'Reactive didactic software',
+    category: 'Clothing',
+    price: 48.94,
+    image: "productImage",
+    description: 'Thing popular raise prove likely spend economic tell fast.'
+}, {
+    id: '14',
+    name: 'Streamlined multi-state synergy',
+    category: 'Electronics',
+    price: 21.6,
+    image: "productImage",
+    description: 'Water security energy employee during vote nation camera.'
+}, {
+    id: '15',
+    name: 'Seamless reciprocal encoding',
+    category: 'Books',
+    price: 181.55,
+    image: "productImage",
+    description: 'Series expect air network forget city arrive green music.'
+}, {
+    id: '16',
+    name: 'Global homogeneous protocol',
+    category: 'Electronics',
+    price: 179.5,
+    image: "productImage",
+    description: 'Enjoy key court fast party trade him suddenly material.'
+}, {
+    id: '17',
+    name: 'Down-sized solution-oriented firmware',
+    category: 'Books',
+    price: 77.72,
+    image: "productImage",
+    description: 'Music perhaps future individual mouth billion north discuss mission.'
+}, {
+    id: '18',
+    name: 'Multi-layered incremental methodology',
+    category: 'Home',
+
+price: 24.17,
+    image: "productImage",
+    description: 'Whether break party crime miss article might want especially.'
+}, {
+    id: '19',
+    name: 'Fully-configurable multimedia forecast',
+    category: 'Clothing',
+    price: 25.17,
+    image: "productImage",
+    description: 'Attack traditional dark trade society admit general admit.'
+}, {
+    id: '20',
+    name: 'Compatible bottom-line benchmark',
+    category: 'Toys',
+    price: 111.79,
+    image: "productImage",
+    description: 'Government score cold sister poor food enjoy green.'
+} ];
